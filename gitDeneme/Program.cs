a3f87323f36b34357699b4f678176186ac338977
thetubiss <87533106+thetubiss@users.noreply.github.com>
2022-08-27 22:01:09 +0300

    Revert "yeni şeyler eklendi"

--- gitDeneme/Program.cs
+++ gitDeneme/Program.cs
@@ -1,6 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Merhaba");
-Console.WriteLine("tuba");
-Console.WriteLine("feride");
-Console.WriteLine("sema");
